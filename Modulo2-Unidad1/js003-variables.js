bd0749bb6cc46893fd268fa73b9da3a9711a98c3
alessitott <david098359@gmail.com>
2025-04-21 20:20:31 -0500

    Creacion del proyecto

---
+++ Modulo2-Unidad1/js003-variables.js
@@ -0,0 +1,12 @@
+console.log("Variables");
+var ciudad="Caracas"
+console.log("Ciudad: ", ciudad );
+var ciudad= "España"
+console.log("segunda ciudad; ", ciudad);
+let pais= "Ecuador"
+console.log("primer pais: " ,pais);
+pais= "Colombia"
+console.log("Segundo pais: ", pais);
+
+const pi = 3.24; /* variable constatnte */
+console.log("Pi : ", pi);
